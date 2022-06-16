1088650b7a186db0584d37baee33d88619f9c9ba
Kadek Rizky Fransisca Putra <rizky.royal@gmail.com>
2022-06-16 14:53:41 +0700

    fixed error

--- Api-Travi-Application/routes/api.php
+++ Api-Travi-Application/routes/api.php
@@ -18,6 +18,10 @@ Route::get('/hello/world', function ($id) {
     
 });
 
+Route::get('/hello/world', function ($id) {
+    
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
