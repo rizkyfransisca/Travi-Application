d37b3131994f2a3462c7f4a5bea42316ad985c21
Kadek Rizky Fransisca Putra <rizky.royal@gmail.com>
2022-06-16 15:02:07 +0700

    test router

--- api-travi-laravel/routes/api.php
+++ api-travi-laravel/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/hello/world', function ($id) {
-    echo 'Hello Word';
+    echo "kadek";
 });
 
 Route::get('/hello/world', function ($id) {
