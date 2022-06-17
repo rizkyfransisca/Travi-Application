81b62e78b665f5e5285778b8b56a7e60fd1e17e3
Kadek Rizky Fransisca Putra <rizky.royal@gmail.com>
2022-06-17 11:09:20 +0700

    Create Tour Package Page

--- travi_app_flutter/lib/main.dart
+++ travi_app_flutter/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travi_app/pages/home_page.dart';
-<<<<<<< HEAD
 import 'package:travi_app/pages/tour_package_page.dart';
-=======
 import 'package:travi_app/pages/event_festival_page.dart';
->>>>>>> a74dc71fae27cb89a6506dfbc8e480f113bd3666
 
 void main() {
   runApp(const MyApp());
@@ -36,8 +33,7 @@ class _NavbarButtonState extends State<NavbarButton> {
     const HomePage(),
     const HomePage(),
     const EventFestivalPage(),
-    const HomePage(),
-    const HomePage(),
+    const TourPackagePage(),
     const HomePage(),
   ];
   
--- travi_app_flutter/lib/pages/tour_package_page.dart
+++ travi_app_flutter/lib/pages/tour_package_page.dart
@@ -1,5 +1,61 @@
 import 'package:flutter/material.dart';
 
+Card cardWidget({image, title}) {
+  return Card(
+    elevation: 4,
+    shadowColor: Colors.grey,
+    clipBehavior: Clip.antiAlias,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          image,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: 90,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 5, left: 8),
+          child: Text(
+            title,
+            style: const TextStyle(
+                color: Colors.black87,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w100),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4, left: 8),
+          child: const Text(
+            'By Kadek Travel',
+            style: TextStyle(
+                fontFamily: 'Poppins', fontSize: 13, color: Colors.grey),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4, left: 8),
+          child: const Text('3 Hari',
+              style: TextStyle(
+                  fontFamily: 'Poppins', fontSize: 13, color: Colors.grey),
+              overflow: TextOverflow.ellipsis),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4, left: 8),
+          child: const Text('Rp 750.000',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins',
+                  fontSize: 13),
+              overflow: TextOverflow.ellipsis),
+        ),
+      ],
+    ),
+  );
+}
+
 class TourPackagePage extends StatefulWidget {
   const TourPackagePage({Key? key}) : super(key: key);
 
@@ -8,118 +64,57 @@ class TourPackagePage extends StatefulWidget {
 }
 
 class _TourPackagePageState extends State<TourPackagePage> {
-  
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Column(
         children: [
           Center(
-            child: Container(
-              margin: const EdgeInsets.only(top: 15),
-              child: const Text(
-                'Tour Package',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'Poppins',
-                    color: Colors.green,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Expanded(
-            child: GridView.count(
-              primary: false,
-              padding: const EdgeInsets.all(10),
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 2,
-              children: <Widget>[
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset('assets/images/kintamani.jpg', fit: BoxFit.cover, width: double.infinity, height: 80,),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6,left: 8),
-                        child: const Text('Keliling Bali Selatan', style: TextStyle(color: Colors.black87, fontFamily: 'Poppins', fontWeight: FontWeight.w100),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('3 Hari', style: TextStyle(fontFamily: 'Poppins', fontSize: 12, color: Colors.grey),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('Rp 750.000', style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Poppins', fontSize: 13)),
-                      ),
-                    ],
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  child: const Text(
+                    'Tour Package',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontFamily: 'Poppins',
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset('assets/images/kintamani.jpg', fit: BoxFit.cover, width: double.infinity, height: 80,),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6,left: 8),
-                        child: const Text('Keliling Bali Selatan', style: TextStyle(color: Colors.black87, fontFamily: 'Poppins', fontWeight: FontWeight.w100),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('3 Hari', style: TextStyle(fontFamily: 'Poppins', fontSize: 12, color: Colors.grey),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('Rp 750.000', style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Poppins', fontSize: 13)),
-                      ),
-                    ],
-                  ),
-                ),
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset('assets/images/kintamani.jpg', fit: BoxFit.cover, width: double.infinity, height: 80,),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6,left: 8),
-                        child: const Text('Keliling Bali Selatan', style: TextStyle(color: Colors.black87, fontFamily: 'Poppins', fontWeight: FontWeight.w100),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('3 Hari', style: TextStyle(fontFamily: 'Poppins', fontSize: 12, color: Colors.grey),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('Rp 750.000', style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Poppins', fontSize: 13)),
-                      ),
-                    ],
-                  ),
-                ),
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset('assets/images/kintamani.jpg', fit: BoxFit.cover, width: double.infinity, height: 80,),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6,left: 8),
-                        child: const Text('Keliling Bali Selatan', style: TextStyle(color: Colors.black87, fontFamily: 'Poppins', fontWeight: FontWeight.w100),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('3 Hari', style: TextStyle(fontFamily: 'Poppins', fontSize: 12, color: Colors.grey),),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 6, left: 8),
-                        child: const Text('Rp 750.000', style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Poppins', fontSize: 13)),
-                      ),
-                    ],
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: const Text(
+                    'Cari paket tour impian mu disini...',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        color: Colors.black),
                   ),
                 ),
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: GridView.builder(
+            primary: false,
+            padding: const EdgeInsets.all(10),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              mainAxisExtent: 220, // here set custom Height You Want
+            ),
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
+              return cardWidget(
+                  image: 'assets/images/kintamani.jpg',
+                  title: 'Keliling Bali Selatan dan Bali Utara');
+            },
+          ))
         ],
       ),
     );
