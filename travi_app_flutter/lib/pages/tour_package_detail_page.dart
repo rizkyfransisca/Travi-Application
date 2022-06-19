fb509c708f289851ce9f4f80d6c80e10a63b2220
Kadek Rizky Fransisca Putra <rizky.royal@gmail.com>
2022-06-19 09:35:38 +0700

    Edit Tour Package Page Detail and Create Tour Package class to mapping data

--- travi_app_flutter/lib/pages/tour_package_detail_page.dart
+++ travi_app_flutter/lib/pages/tour_package_detail_page.dart
@@ -16,67 +16,189 @@ class _TourPackageDetailState extends State<TourPackageDetail> {
 
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.black
-        ),
+        iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
         title: const Text(
-          'Event & Festival',
+          'Tour Package',
           style: TextStyle(
-            color: Colors.green,
-            fontSize: 20,
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.bold
-          ),
+              color: Colors.green,
+              fontSize: 20,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            arguments.image,
-            width: MediaQuery.of(context).size.width,
-            height: 250,
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  arguments.title,
-                  style: const TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                    letterSpacing: 0.6
-                  ),
-                ),
-                const SizedBox(height: 12),
-                Text(
-                  'Lokasi : ' + arguments.title,
-                  style: const TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w300,
-                    fontSize: 13,
-                    letterSpacing: 0.4
-                  ),
-                ),
-                Text(
-                  arguments.price,
-                  style: const TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w300,
-                    fontSize: 13,
-                    letterSpacing: 0.4
-                  ),
-                )
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              arguments.image,
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            const SizedBox(
+              height: 12,
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 15),
+              child: PhysicalModel(
+                elevation: 2,
+                color: Colors.white,
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          arguments.packageName,
+                          style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              letterSpacing: 0.6),
+                        ),
+                        const SizedBox(height: 12),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Nama Travel : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.travelName),
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Durasi : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.duration)
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Harga : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.price)
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Kegiatan Wisata : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.activity),
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Pelayanan : ', style: TextStyle(fontWeight: FontWeight.bold) ),
+                              TextSpan(text: arguments.service),
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Email : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.email),
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'No Telepon : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.phoneNumber),
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 3),
+                        Text.rich(
+                          TextSpan(
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              letterSpacing: 0.4),
+                            children: [
+                              const TextSpan(text: 'Alamat : ', style: TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(text: arguments.address),
+                            ]
+                          )
+                        ),
+                        const SizedBox(height: 26),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Padding(
+                            padding:
+                                EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                            child: Text(
+                              'Mulai Sekarang',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, letterSpacing: 0.5),
+                            ),
+                          ),
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4)))),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- travi_app_flutter/lib/pages/tour_package_page.dart
+++ travi_app_flutter/lib/pages/tour_package_page.dart
@@ -1,7 +1,18 @@
-
 import 'package:flutter/material.dart';
 
-Card cardWidget({image, title, travelName, period, price, context}) {
+Card cardWidget(
+    {image,
+    packageName,
+    travelName,
+    duration,
+    price,
+    email,
+    phoneNumber,
+    address,
+    activity,
+    description,
+    service,
+    context}) {
   return Card(
     elevation: 3,
     shadowColor: Colors.grey,
@@ -9,8 +20,7 @@ Card cardWidget({image, title, travelName, period, price, context}) {
     child: InkWell(
       onTap: () {
         Navigator.pushNamed(context, '/tour-package/detail',
-            arguments:
-                TourPackageArguments(image, title, travelName, period, price));
+            arguments: TourPackageArguments(image, packageName, travelName, duration, price, email, phoneNumber, address, activity, description, service));
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -24,7 +34,7 @@ Card cardWidget({image, title, travelName, period, price, context}) {
           Container(
             margin: const EdgeInsets.only(top: 5, left: 8),
             child: Text(
-              title,
+              packageName,
               style: const TextStyle(
                   color: Colors.black87,
                   fontFamily: 'Poppins',
@@ -44,7 +54,7 @@ Card cardWidget({image, title, travelName, period, price, context}) {
           ),
           Container(
             margin: const EdgeInsets.only(top: 4, left: 8),
-            child: Text(period,
+            child: Text(duration,
                 style: const TextStyle(
                     fontFamily: 'Poppins', fontSize: 13, color: Colors.grey),
                 overflow: TextOverflow.ellipsis),
@@ -66,13 +76,29 @@ Card cardWidget({image, title, travelName, period, price, context}) {
 
 class TourPackageArguments {
   final String image;
-  final String title;
+  final String packageName;
   final String travelName;
-  final String period;
+  final String duration;
   final String price;
+  final String email;
+  final String phoneNumber;
+  final String address;
+  final String activity;
+  final String description;
+  final String service;
 
   TourPackageArguments(
-      this.image, this.title, this.travelName, this.period, this.price);
+      this.image,
+      this.packageName,
+      this.travelName,
+      this.duration,
+      this.price,
+      this.email,
+      this.phoneNumber,
+      this.address,
+      this.activity,
+      this.description,
+      this.service);
 }
 
 class TourPackagePage extends StatefulWidget {
@@ -90,7 +116,7 @@ class _TourPackagePageState extends State<TourPackagePage> {
     'assets/images/bedugul.jpg',
   ];
 
-  var titles = [
+  var packageNames = [
     'Keliling Bali Selatan dan Bali Timur',
     'Keliling Bali Selatan dan Bali Barat',
     'Keliling Bali',
@@ -104,9 +130,51 @@ class _TourPackagePageState extends State<TourPackagePage> {
     'Travel Kuy'
   ];
 
-  var periods = ['3 Hari', '1 Minggu', '5 Hari', '6 Hari'];
+  var durations = ['3 Hari', '1 Minggu', '5 Hari', '6 Hari'];
 
   var prices = ['Rp 750.000', 'Rp 850.000', 'Rp 400.000', 'Rp 520.000'];
+
+  var phoneNumbers = [
+    '0128392108321038123',
+    '123981238123',
+    '1293812938',
+    '12391239712372193'
+  ];
+
+  var emails = [
+    'rizky.royal@gmail.com',
+    'agusseptyawan@gmail.com',
+    'raflycincah@gmail.com',
+    'rizkykhoi@gmail.com'
+  ];
+
+  var addresses = [
+    'Desa Munduk, Tabanan',
+    'Desa Penglipuran',
+    'Desa Subang',
+    'Desa JatiWaringin'
+  ];
+
+  var activities = [
+    'Berwisata ke kebun raya, berwisata mancing, berwisata berenang',
+    'Bermain bersama lumba-lumba, ke kebun binatang, launch, berenang',
+    'Naik kuda, naik gajah, mendaki gunung, bermain air',
+    'Surfing, mancing ikan hiu, berperahu, bermain main'
+  ];
+
+  var services = [
+    'Makan siang, makan malam, makan pagi',
+    'Mobil mewah, makan malam, makan pagi',
+    'Hotel bintang 5, makan malam, makan pagi',
+    'Makan siang, mobil mercedes, makan pagi',
+  ];
+
+  var descriptions = [
+    'Paket wisata ini sangat cocok bagi kalian yang sangat sumpek dengan pekerjaan duniawi. Pada paket wisata ini, kita akan mengeksplor Bali bagian utara dengan keindahan alam nya dan kesejukan gunung-gunung nya. Pada paket wisata ini, kita juga akan mengunjungi desa-desa kuno yang ada di Bali utara. Selain itu, kita juga akan melakukan <em>chill&nbsp;</em>di coffee shop yang ada di daerah Kintamani, tentu saja disini kalian juga bisa berswafoto dengan latar gunung batur yang sangat cantik dan menawan. Dan tidak kalah penting, pada paket wisata ini kita juga mengunjungi salah satu kebun raya terbesar yang ada di Bali.',
+    'Paket wisata ini sangat cocok bagi kalian yang sangat sumpek dengan pekerjaan duniawi. Pada paket wisata ini, kita akan mengeksplor Bali bagian utara dengan keindahan alam nya dan kesejukan gunung-gunung nya. Pada paket wisata ini, kita juga akan mengunjungi desa-desa kuno yang ada di Bali utara. Selain itu, kita juga akan melakukan <em>chill&nbsp;</em>di coffee shop yang ada di daerah Kintamani, tentu saja disini kalian juga bisa berswafoto dengan latar gunung batur yang sangat cantik dan menawan. Dan tidak kalah penting, pada paket wisata ini kita juga mengunjungi salah satu kebun raya terbesar yang ada di Bali.',
+    'Paket wisata ini sangat cocok bagi kalian yang sangat sumpek dengan pekerjaan duniawi. Pada paket wisata ini, kita akan mengeksplor Bali bagian utara dengan keindahan alam nya dan kesejukan gunung-gunung nya. Pada paket wisata ini, kita juga akan mengunjungi desa-desa kuno yang ada di Bali utara. Selain itu, kita juga akan melakukan <em>chill&nbsp;</em>di coffee shop yang ada di daerah Kintamani, tentu saja disini kalian juga bisa berswafoto dengan latar gunung batur yang sangat cantik dan menawan. Dan tidak kalah penting, pada paket wisata ini kita juga mengunjungi salah satu kebun raya terbesar yang ada di Bali.',
+    'Paket wisata ini sangat cocok bagi kalian yang sangat sumpek dengan pekerjaan duniawi. Pada paket wisata ini, kita akan mengeksplor Bali bagian utara dengan keindahan alam nya dan kesejukan gunung-gunung nya. Pada paket wisata ini, kita juga akan mengunjungi desa-desa kuno yang ada di Bali utara. Selain itu, kita juga akan melakukan <em>chill&nbsp;</em>di coffee shop yang ada di daerah Kintamani, tentu saja disini kalian juga bisa berswafoto dengan latar gunung batur yang sangat cantik dan menawan. Dan tidak kalah penting, pada paket wisata ini kita juga mengunjungi salah satu kebun raya terbesar yang ada di Bali.',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -157,10 +225,16 @@ class _TourPackagePageState extends State<TourPackagePage> {
             itemBuilder: (BuildContext context, int index) {
               return cardWidget(
                   image: images[index],
-                  title: titles[index],
+                  packageName: packageNames[index],
                   travelName: travelNames[index],
-                  period: periods[index],
+                  duration: durations[index],
                   price: prices[index],
+                  email: emails[index],
+                  phoneNumber: phoneNumbers[index],
+                  address: addresses[index],
+                  activity: activities[index],
+                  description: descriptions[index],
+                  service: services[index],
                   context: context);
             },
           ))
