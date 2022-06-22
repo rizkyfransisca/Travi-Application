9251f93ee1c18a4efa8a5d7f63f4f1029c14c6c8
krishnavrindavana <krishna.vrindavana@yahoo.com>
2022-06-22 23:30:59 +0800

    menambahkan join_us

--- travi_app_flutter/lib/pages/join_us_page.dart
+++ travi_app_flutter/lib/pages/join_us_page.dart
@@ -1,30 +1,159 @@
 import 'package:flutter/material.dart';
 
 class JoinUsPage extends StatefulWidget {
-  const JoinUsPage({ Key? key }) : super(key: key);
+  const JoinUsPage({Key? key}) : super(key: key);
 
   @override
   State<JoinUsPage> createState() => _JoinUsPageState();
 }
 
 class _JoinUsPageState extends State<JoinUsPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Center(
-          child: Text(
-            'Join Us',
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 20,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Center(
+            child: Text(
+              'Join Us',
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 20,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold),
             ),
           ),
         ),
-      ),
-    );
+        body: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama/Nama Perusahaan tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Nama/Nama Perusahaan',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama Paket Tour tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Nama Paket Tour',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'No.Telepon tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'No.Telepon',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Alamat tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Alamat',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Kegiatan Wisata tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Kegiatan Wisata',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Deskripsi tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Deskripsi',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Pelayanan tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Pelayanan',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Harga tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Harga',
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        showDialog(
+                            context: context,
+                            builder: (ctx) {
+                              return AlertDialog(
+                                content:
+                                    Text("Selamat Datang ${myController.text}"),
+                              );
+                            });
+                      }
+                    },
+                    child: const Text('Mulai Sekarang'))
+              ],
+            )));
   }
 }
